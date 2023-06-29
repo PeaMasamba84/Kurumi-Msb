586e39da081764c6d0672d3218c18bc5fa8a72fa
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-06-29 09:30:26 +0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -71,7 +71,7 @@ class TelegraphHelper:
                     nxt_page += 1
             await self.edit_page(
                 path=path[prev_page],
-                title='KQRM Mirror Search',
+                title='PEA MASAMBA Mirror Search',
                 content=content
             )
         return
