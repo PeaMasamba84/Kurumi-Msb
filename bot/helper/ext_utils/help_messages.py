847a8b00f9cb654dd2007c2b4f6d593aa13bf59c
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-06-29 22:21:59 +0700

    Update help_messages.py

--- bot/helper/ext_utils/help_messages.py
+++ bot/helper/ext_utils/help_messages.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
+# TODO : Change to efisien help message, Not much variable in help_message.py
 
 YT_HELP_MESSAGE = f"""
 <b>Send link along with command line</b>:
@@ -62,7 +63,6 @@ Reply to this example by this cmd <code>/{BotCommands.YtdlCommand[0]}</code> b(b
 You can set start and end of the links from the bulk with -b start:end or only end by -b :end or only start by -b start. The default start is from zero(first link) to inf.
 
 Check all yt-dlp api options from this <a href='https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py#L184'>FILE</a> or use this <a href='https://t.me/mltb_official_channel/177'>script</a> to convert cli arguments to api options.
-
 <b>PEA MASAMBA</b>
 """
 
@@ -152,7 +152,273 @@ Super: <code>https://t.me/c/channel_id/message_id</code>
 
 <b>NOTES:</b>
 1. Commands that start with <b>qb</b> are ONLY for torrents.
+<b>PEA MASAMBA</b>
+"""
 
+LEECH_HELP_MESSAGE = f"""
+<code>/{BotCommands.LeechCommand[0]}</code> link -n new name
+
+<b>By replying to link/file</b>:
+<code>/{BotCommands.LeechCommand[0]}</code> -n new name -z -e -up upload destination
+
+<b>New Name</b>: -n
+<code>/{BotCommands.LeechCommand[0]}</code> link -n new name
+Note: Doesn't work with torrents.
+
+<b>Direct link authorization</b>: -au -ap
+<code>/{BotCommands.LeechCommand[0]}</code> link -au username -ap password
+
+<b>Extract/Zip</b>: -e -z
+<code>/{BotCommands.LeechCommand[0]}</code> link -e password (extract password protected)
+<code>/{BotCommands.LeechCommand[0]}</code> link -z password (zip password protected)
+<code>/{BotCommands.LeechCommand[0]}</code> link -z password -e (extract and zip password protected)
+<code>/{BotCommands.LeechCommand[0]}</code> link -e password -z password (extract password protected and zip password protected)
+Note: When both extract and zip added with cmd it will extract first and then zip, so always extract first
+
+<b>Bittorrent selection</b>: -s
+<code>/{BotCommands.LeechCommand[0]}</code> link -s or by replying to file/link
+
+<b>Bittorrent seed</b>: -d
+<code>/{BotCommands.LeechCommand[0]}</code> link -d ratio:seed_time or by replying to file/link
+To specify ratio and seed time add -d ratio:time. Ex: -d 0.7:10 (ratio and time) or -d 0.7 (only ratio) or -d :10 (only time) where time in minutes.
+
+<b>Multi links only by replying to first link/file</b>: -i
+<code>/{BotCommands.LeechCommand[0]}</code> -i 10(number of links/files)
+
+<b>Multi links within same upload directory only by replying to first link/file</b>: -m
+<code>/{BotCommands.LeechCommand[0]}</code> -i 10(number of links/files) -m folder name (multi message)
+<code>/{BotCommands.LeechCommand[0]}</code> -b -m folder name (bulk-message/file)
+
+<b>Upload</b>: -up
+<code>/{BotCommands.LeechCommand[0]}</code> link -up <code>rcl</code> (To select rclone config, remote & path or Tg id/username)
+You can directly add the upload path: -up remote:dir/subdir
+If DEFAULT_UPLOAD is `rc` then you can pass up: `gd` to upload using gdrive tools to GDRIVE_ID.
+If DEFAULT_UPLOAD is `gd` then you can pass up: `rc` to upload to RCLONE_PATH.
+If you want to add path manually from your config (uploaded from usetting) add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.LeechCommand[0]}</code> link -up <code>mrcc:</code>main:dump
+DEFAULT_UPLOAD doesn't effect on leech cmds.
+<code>{BotCommands.LeechCommand[0]}</code> link -up chat_id or username
+
+<b>Rclone Flags</b>: -rcf
+<code>/{BotCommands.LeechCommand[0]}</code> link|path|rcl -up path|rcl -rcf --buffer-size:8M|--drive-starred-only|key|key:value
+This will override all other flags except --exclude
+Check here all <a href='https://rclone.org/flags/'>RcloneFlags</a>.
+
+<b>Bulk Download</b>: -b
+Bulk can be used by text message and by replying to text file contains links seperated by new line.
+You can use it only by reply to message(text/file).
+All options should be along with link!
+Example:
+link1 -n new name -up remote1:path1 -rcf |key:value|key:value
+link2 -z -n new name -up remote2:path2
+link3 -e -n new name -up remote2:path2
+Note: You can't add -m arg for some links only, do it for all links or use multi without bulk!
+Reply to this example by this cmd <code>/{BotCommands.LeechCommand[0]}</code> -b(bulk)
+You can set start and end of the links from the bulk like seed, with -b start:end or only end by -b :end or only start by -b start. The default start is from zero(first link) to inf.
+
+<b>Join Splitted Files</b>: -j
+This option will only work before extract and zip, so mostly it will be used with -m argument (samedir)
+By Reply:
+<code>/{BotCommands.LeechCommand[0]}</code> -i 3 -j -m folder name
+<code>/{BotCommands.LeechCommand[0]}</code> -b -j -m folder name
+if u have link have splitted files:
+<code>/{BotCommands.LeechCommand[0]}</code> link -j
+
+<b>Rclone Download</b>:
+Treat rclone paths exactly like links
+<code>/{BotCommands.LeechCommand[0]}</code> main:dump/ubuntu.iso or <code>rcl</code>(To select config, remote and path)
+Users can add their own rclone from user settings
+If you want to add path manually from your config add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.LeechCommand[0]}</code> <code>mrcc:</code>main:dump/ubuntu.iso
+
+<b>TG Links</b>:
+Treat links like any direct link
+Some links need user access so sure you must add USER_SESSION_STRING for it.
+Three types of links:
+Public: <code>https://t.me/channel_name/message_id</code>
+Private: <code>tg://openmessage?user_id=xxxxxx&message_id=xxxxx</code>
+Super: <code>https://t.me/c/channel_id/message_id</code>
+
+<b>NOTES:</b>
+1. Commands that start with <b>qb</b> are ONLY for torrents.
+<b>PEA MASAMBA</b>
+"""
+
+QBMIRROR_HELP_MESSAGE = f"""
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -n new name
+
+<b>By replying to link/file</b>:
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -n new name -z -e -up upload destination
+
+<b>New Name</b>: -n
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -n new name
+Note: Doesn't work with torrents.
+
+<b>Direct link authorization</b>: -au -ap
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -au username -ap password
+
+<b>Extract/Zip</b>: -e -z
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -e password (extract password protected)
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -z password (zip password protected)
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -z password -e (extract and zip password protected)
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -e password -z password (extract password protected and zip password protected)
+Note: When both extract and zip added with cmd it will extract first and then zip, so always extract first
+
+<b>Bittorrent selection</b>: -s
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -s or by replying to file/link
+
+<b>Bittorrent seed</b>: -d
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -d ratio:seed_time or by replying to file/link
+To specify ratio and seed time add -d ratio:time. Ex: -d 0.7:10 (ratio and time) or -d 0.7 (only ratio) or -d :10 (only time) where time in minutes.
+
+<b>Multi links only by replying to first link/file</b>: -i
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -i 10(number of links/files)
+
+<b>Multi links within same upload directory only by replying to first link/file</b>: -m
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -i 10(number of links/files) -m folder name (multi message)
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -b -m folder name (bulk-message/file)
+
+<b>Upload</b>: -up
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -up <code>rcl</code> (To select rclone config, remote & path or Tg id/username)
+You can directly add the upload path: -up remote:dir/subdir
+If DEFAULT_UPLOAD is `rc` then you can pass up: `gd` to upload using gdrive tools to GDRIVE_ID.
+If DEFAULT_UPLOAD is `gd` then you can pass up: `rc` to upload to RCLONE_PATH.
+If you want to add path manually from your config (uploaded from usetting) add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -up <code>mrcc:</code>main:dump
+DEFAULT_UPLOAD doesn't effect on leech cmds.
+<code>{BotCommands.LeechCommand[0]}</code> link -up chat_id or username
+
+<b>Rclone Flags</b>: -rcf
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link|path|rcl -up path|rcl -rcf --buffer-size:8M|--drive-starred-only|key|key:value
+This will override all other flags except --exclude
+Check here all <a href='https://rclone.org/flags/'>RcloneFlags</a>.
+
+<b>Bulk Download</b>: -b
+Bulk can be used by text message and by replying to text file contains links seperated by new line.
+You can use it only by reply to message(text/file).
+All options should be along with link!
+Example:
+link1 -n new name -up remote1:path1 -rcf |key:value|key:value
+link2 -z -n new name -up remote2:path2
+link3 -e -n new name -up remote2:path2
+Note: You can't add -m arg for some links only, do it for all links or use multi without bulk!
+Reply to this example by this cmd <code>/{BotCommands.QbMirrorCommand[0]}</code> -b(bulk)
+You can set start and end of the links from the bulk like seed, with -b start:end or only end by -b :end or only start by -b start. The default start is from zero(first link) to inf.
+
+<b>Join Splitted Files</b>: -j
+This option will only work before extract and zip, so mostly it will be used with -m argument (samedir)
+By Reply:
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -i 3 -j -m folder name
+<code>/{BotCommands.QbMirrorCommand[0]}</code> -b -j -m folder name
+if u have link have splitted files:
+<code>/{BotCommands.QbMirrorCommand[0]}</code> link -j
+
+<b>Rclone Download</b>:
+Treat rclone paths exactly like links
+<code>/{BotCommands.QbMirrorCommand[0]}</code> main:dump/ubuntu.iso or <code>rcl</code>(To select config, remote and path)
+Users can add their own rclone from user settings
+If you want to add path manually from your config add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.QbMirrorCommand[0]}</code> <code>mrcc:</code>main:dump/ubuntu.iso
+
+<b>TG Links</b>:
+Treat links like any direct link
+Some links need user access so sure you must add USER_SESSION_STRING for it.
+Three types of links:
+Public: <code>https://t.me/channel_name/message_id</code>
+Private: <code>tg://openmessage?user_id=xxxxxx&message_id=xxxxx</code>
+Super: <code>https://t.me/c/channel_id/message_id</code>
+
+<b>NOTES:</b>
+1. Commands that start with <b>qb</b> are ONLY for torrents.
+<b>PEA MASAMBA</b>
+"""
+
+QBLEECH_HELP_MESSAGE = f"""
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -n new name
+
+<b>By replying to link/file</b>:
+<code>/{BotCommands.QbLeechCommand[0]}</code> -n new name -z -e -up upload destination
+
+<b>New Name</b>: -n
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -n new name
+Note: Doesn't work with torrents.
+
+<b>Direct link authorization</b>: -au -ap
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -au username -ap password
+
+<b>Extract/Zip</b>: -e -z
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -e password (extract password protected)
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -z password (zip password protected)
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -z password -e (extract and zip password protected)
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -e password -z password (extract password protected and zip password protected)
+Note: When both extract and zip added with cmd it will extract first and then zip, so always extract first
+
+<b>Bittorrent selection</b>: -s
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -s or by replying to file/link
+
+<b>Bittorrent seed</b>: -d
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -d ratio:seed_time or by replying to file/link
+To specify ratio and seed time add -d ratio:time. Ex: -d 0.7:10 (ratio and time) or -d 0.7 (only ratio) or -d :10 (only time) where time in minutes.
+
+<b>Multi links only by replying to first link/file</b>: -i
+<code>/{BotCommands.QbLeechCommand[0]}</code> -i 10(number of links/files)
+
+<b>Multi links within same upload directory only by replying to first link/file</b>: -m
+<code>/{BotCommands.QbLeechCommand[0]}</code> -i 10(number of links/files) -m folder name (multi message)
+<code>/{BotCommands.QbLeechCommand[0]}</code> -b -m folder name (bulk-message/file)
+
+<b>Upload</b>: -up
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -up <code>rcl</code> (To select rclone config, remote & path or Tg id/username)
+You can directly add the upload path: -up remote:dir/subdir
+If DEFAULT_UPLOAD is `rc` then you can pass up: `gd` to upload using gdrive tools to GDRIVE_ID.
+If DEFAULT_UPLOAD is `gd` then you can pass up: `rc` to upload to RCLONE_PATH.
+If you want to add path manually from your config (uploaded from usetting) add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -up <code>mrcc:</code>main:dump
+DEFAULT_UPLOAD doesn't effect on leech cmds.
+<code>{BotCommands.LeechCommand[0]}</code> link -up chat_id or username
+
+<b>Rclone Flags</b>: -rcf
+<code>/{BotCommands.QbLeechCommand[0]}</code> link|path|rcl -up path|rcl -rcf --buffer-size:8M|--drive-starred-only|key|key:value
+This will override all other flags except --exclude
+Check here all <a href='https://rclone.org/flags/'>RcloneFlags</a>.
+
+<b>Bulk Download</b>: -b
+Bulk can be used by text message and by replying to text file contains links seperated by new line.
+You can use it only by reply to message(text/file).
+All options should be along with link!
+Example:
+link1 -n new name -up remote1:path1 -rcf |key:value|key:value
+link2 -z -n new name -up remote2:path2
+link3 -e -n new name -up remote2:path2
+Note: You can't add -m arg for some links only, do it for all links or use multi without bulk!
+Reply to this example by this cmd <code>/{BotCommands.QbLeechCommand[0]}</code> -b(bulk)
+You can set start and end of the links from the bulk like seed, with -b start:end or only end by -b :end or only start by -b start. The default start is from zero(first link) to inf.
+
+<b>Join Splitted Files</b>: -j
+This option will only work before extract and zip, so mostly it will be used with -m argument (samedir)
+By Reply:
+<code>/{BotCommands.QbLeechCommand[0]}</code> -i 3 -j -m folder name
+<code>/{BotCommands.QbLeechCommand[0]}</code> -b -j -m folder name
+if u have link have splitted files:
+<code>/{BotCommands.QbLeechCommand[0]}</code> link -j
+
+<b>Rclone Download</b>:
+Treat rclone paths exactly like links
+<code>/{BotCommands.QbLeechCommand[0]}</code> main:dump/ubuntu.iso or <code>rcl</code>(To select config, remote and path)
+Users can add their own rclone from user settings
+If you want to add path manually from your config add <code>mrcc:</code> before the path without space
+<code>/{BotCommands.QbLeechCommand[0]}</code> <code>mrcc:</code>main:dump/ubuntu.iso
+
+<b>TG Links</b>:
+Treat links like any direct link
+Some links need user access so sure you must add USER_SESSION_STRING for it.
+Three types of links:
+Public: <code>https://t.me/channel_name/message_id</code>
+Private: <code>tg://openmessage?user_id=xxxxxx&message_id=xxxxx</code>
+Super: <code>https://t.me/c/channel_id/message_id</code>
+
+<b>NOTES:</b>
+1. Commands that start with <b>qb</b> are ONLY for torrents.
 <b>PEA MASAMBA</b>
 """
 
@@ -177,7 +443,6 @@ Filter Notes:
 3. You can add `or` and `|` as much as you want."
 4. Take look on title if it has static special character after or before the qualities or extensions or whatever and use them in filter to avoid wrong match.
 Timeout: 60 sec.
-
 <b>PEA MASAMBA</b>
 """
 
@@ -194,6 +459,5 @@ Send Gdrive|Gdot|Filepress|Filebee|Appdrive|Gdflix link or rclone path along wit
 <code>/{BotCommands.CloneCommand[0]}</code> (rcl or rclone_path) -up (rcl or rclone_path) -rcf flagkey:flagvalue|flagkey|flagkey:flagvalue
 
 Note: If -up not specified then rclone destination will be the RCLONE_PATH from config.env
-
 <b>PEA MASAMBA</b>
 """
