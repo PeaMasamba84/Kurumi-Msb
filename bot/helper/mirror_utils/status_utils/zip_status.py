5e0bad7495e19c194ec31f02ca0fb62a05faef05
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-08-14 15:15:28 +0700

    Update zip_status.py

--- bot/helper/mirror_utils/status_utils/zip_status.py
+++ bot/helper/mirror_utils/status_utils/zip_status.py
@@ -1,71 +1,51 @@
 #!/usr/bin/env python3
-from time import time
-
-from bot import LOGGER
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time, async_to_sync
+from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time, async_to_sync
 from bot.helper.ext_utils.fs_utils import get_path_size
 
 
-class ZipStatus:
-    def __init__(self, name, size, gid, listener):
-        self.__name = name
-        self.__size = size
-        self.__gid = gid
+class YtDlpDownloadStatus:
+    def __init__(self, obj, listener, gid):
+        self.__obj = obj
         self.__listener = listener
-        self.__uid = listener.uid
-        self.__start_time = time()
-        self.message = listener.message
+        self.__gid = gid
+        self.message = self.__listener.message
 
     def gid(self):
         return self.__gid
 
-    def speed_raw(self):
-        return self.processed_raw() / (time() - self.__start_time)
+    def processed_bytes(self):
+        return get_readable_file_size(self.processed_raw())
 
-    def progress_raw(self):
-        try:
-            return self.processed_raw() / self.__size * 100
-        except:
-            return 0
-
-    def progress(self):
-        return f'{round(self.progress_raw(), 2)}%'
-
-    def speed(self):
-        return f'{get_readable_file_size(self.speed_raw())}/s'
-
-    def name(self):
-        return self.__name
+    def processed_raw(self):
+        if self.__obj.downloaded_bytes != 0:
+            return self.__obj.downloaded_bytes
+        else:
+            return async_to_sync(get_path_size, self.__listener.dir)
 
     def size(self):
-        return get_readable_file_size(self.__size)
+        return get_readable_file_size(self.__obj.size)
+
+    def status(self):
+        return MirrorStatus.STATUS_DOWNLOADING
+
+    def name(self):
+        return self.__obj.name
+
+    def progress(self):
+        return f'{round(self.__obj.progress, 2)}%'
+
+    def speed(self):
+        return f'{get_readable_file_size(self.__obj.download_speed)}/s'
 
     def eta(self):
+        if self.__obj.eta != '-':
+            return get_readable_time(self.__obj.eta)
         try:
-            seconds = (self.__size - self.processed_raw()) / self.speed_raw()
+            seconds = (self.__obj.size - self.processed_raw()) / \
+                self.__obj.download_speed
             return get_readable_time(seconds)
         except:
             return '-'
 
-    def status(self):
-        return MirrorStatus.STATUS_ARCHIVING
-
-    def processed_raw(self):
-        if self.__listener.newDir:
-            return async_to_sync(get_path_size, self.__listener.newDir)
-        else:
-            return async_to_sync(get_path_size, self.__listener.dir) - self.__size
-
-    def processed_bytes(self):
-        return get_readable_file_size(self.processed_raw())
-
     def download(self):
-        return self
-
-    async def cancel_download(self):
-        LOGGER.info(f'Cancelling Archive: {self.__name}')
-        if self.__listener.suproc is not None:
-            self.__listener.suproc.kill()
-        else:
-            self.__listener.suproc = 'cancelled'
-        await self.__listener.onUploadError('Archive dibatalkan oleh User!')
+        return self.__obj
