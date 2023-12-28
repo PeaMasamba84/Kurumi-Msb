1edc266e8a5d6b3eb535f0ae78b0af6e3bb31712
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-12-28 19:14:59 +0700

    fix

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -275,7 +275,7 @@ class TaskListener(TaskConfig):
             ):
                 buttons = ButtonMaker()
                 if link:
-                    buttons.ubutton("☁️ Cloud Link", link)
+                    buttons.ubutton("Cloud Link", link)
                 if rclonePath:
                     msg += f"\n\n<b>Path :</b> <code>{rclonePath}</code>"
                 if (
@@ -288,7 +288,7 @@ class TaskListener(TaskConfig):
                     share_url = f"{RCLONE_SERVE_URL}/{remote}/{url_path}"
                     if mime_type == "Folder":
                         share_url += "/"
-                    buttons.ubutton("🔗 Rclone Link", share_url)
+                    buttons.ubutton("Rclone Link", share_url)
                 if not rclonePath and dir_id:
                     INDEX_URL = ""
                     if self.privateLink:
@@ -301,10 +301,10 @@ class TaskListener(TaskConfig):
                         INDEX_URL = config_dict["INDEX_URL"]
                     if INDEX_URL:
                         share_url = f"{INDEX_URL}findpath?id={dir_id}"
-                        buttons.ubutton("⚡ Index Link", share_url)
+                        buttons.ubutton("Index Link", share_url)
                         if mime_type.startswith(("image", "video", "audio")):
                             share_urls = f"{INDEX_URL}findpath?id={dir_id}&view=true"
-                            buttons.ubutton("🌐 View Link", share_urls)
+                            buttons.ubutton("View Link", share_urls)
                 button = buttons.build_menu(2)
             else:
                 msg += f"\n\n<b>Path :</b> <code>{rclonePath}</code>"
