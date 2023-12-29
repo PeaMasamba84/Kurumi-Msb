f4f2b2cc75b5e6c817707525809643b298716c68
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-12-29 09:24:34 +0700

    fix

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -147,11 +147,11 @@ async def status_pages(_, query):
 EX : {tasks['Extract']} | SP : {tasks['Split']} | QD : {tasks['QueueDl']} | QU : {tasks['QueueUp']}
 CL : {tasks['Clone']} | CH : {tasks['CheckUp']} | PA : {tasks['Pause']} | SV : {tasks['SamVid']}
 
-Kec. Seed : {get_readable_file_size(seed_speed)}/s
-Kec. Unduh : {get_readable_file_size(dl_speed)}/s
-Kec. Unggah : {get_readable_file_size(up_speed)}/s
+🌾𝑺𝒑𝒆𝒆𝒅 𝑺𝒆𝒆𝒅 : {get_readable_file_size(seed_speed)}/s
+📤𝑺𝒑𝒆𝒆𝒅 𝑼𝒏𝒅𝒖𝒉 : {get_readable_file_size(dl_speed)}/s
+📥𝑺𝒑𝒆𝒆𝒅 𝑼𝒏𝒈𝒈𝒂𝒉 : {get_readable_file_size(up_speed)}/s
 
-@{bot.me.username}
+By: 🅿🅴🅰 🅼🅰🆂🅰🅼🅱🅰
 """
         await query.answer(msg, show_alert=True)
 
