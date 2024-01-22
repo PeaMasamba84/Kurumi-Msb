b8f093a1564c8e459cf6fb718645cb649690bb93
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2024-01-22 20:59:54 +0700

    fix

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -145,7 +145,7 @@ def get_readable_message(sid, is_user, page_no=1, status="All", page_step=1):
             msg += f"\n💾 File :<blockquote><code>PRIVATE</code></blockquote></b>"
         else: 
             msg += f"\n💾 File :<blockquote><code>{escape(f'{task.name()}')}</code></blockquote>\n"
-        msg += f"\n<b>┌┤{get_progress_bar_string(task.progress())} <code>{task.progress()}</code></b>"
+        msg += f"\n<b>┌┤{get_progress_bar_string(task.progress())} <code>{task.progress()}</code>»</b>"
         if task.listener.isSuperChat:
             msg += f"\n<b>├📲 Status :</b> <a href='{task.listener.message.link}'>{tstatus}</a>"
         else:
