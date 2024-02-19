51581ccb32f679f949430b8b110af6c3bd6b38af
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2024-02-19 13:07:31 +0700

    Fix for JDownloader

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -17,7 +17,7 @@ from bot.helper.telegram_helper.message_utils import (
 
 @new_task
 async def speedtest(_, message):
-    msg = await sendMessage(message, "<b>Mengetes kecepatan Unduh & Unggah...</b>")
+    msg = await sendMessage(message, "<b>Tes kecepatan Unduh & Unggah...</b>")
     test = Speedtest()
     test.get_best_server()
     test.download()
@@ -25,24 +25,28 @@ async def speedtest(_, message):
     test.results.share()
     result = test.results.dict()
     caption = f"""
+<b>🆂🅿🅴🅴🅳🆃🅴🆂🆃</b>
 <pre languange='bash'>
-<b>Hasil SpeedTest</b>
 <b>Ping         :</b> <code>{result['ping']} ms</code>
 <b>Waktu        :</b> <code>{result['timestamp']}</code>
 <b>Unggah       :</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
 <b>Upload       :</b> <code>{get_readable_file_size(result['download'] / 8)}/s</code>
 <b>Terkirim     :</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
 <b>Diterima     :</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+</pre>
 
-<b>Client</b>
+<b>🅸🅽🅵🅾 🅲🅻🅸🅴🅽🆃</b>
+<pre languange='bash'>
 <b>IP           :</b> <code>{result['client']['ip']}</code>
 <b>Nama         :</b> <code>{result['client']['isp']}</code>
 <b>Rating       :</b> <code>{result['client']['isprating']}</code>
 <b>Negara       :</b> <code>{result['client']['country']}</code>
 <b>Latitude     :</b> <code>{result['client']['lat']}</code>
 <b>Longitude    :</b> <code>{result['client']['lon']}</code>
+</pre>
 
-<b>Server</b>
+<b>🅸🅽🅵🅾 🆂🅴🆁🆅🅴🆁</b>
+<pre languange='bash'>
 <b>Nama         :</b> <code>{result['server']['name']}</code>
 <b>Sponsor      :</b> <code>{result['server']['sponsor']}</code>
 <b>Latency      :</b> <code>{result['server']['latency']}</code>
