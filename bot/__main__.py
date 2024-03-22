d625e7352482c6108ac42821d5ed664c9c786d5a
Pea Masamba <104630285+PeaMasamba84@users.noreply.github.com>
2024-03-22 12:34:08 +0700

    update time

--- bot/__main__.py
+++ bot/__main__.py
@@ -332,7 +332,7 @@ async def restart_notification():
         except Exception as e:
             LOGGER.error(e)
 
-    now = datetime.now(timezone("Asia/Jakarta"))
+    now = datetime.now(timezone("Asia/Makassar"))
     if INCOMPLETE_TASK_NOTIFIER and DATABASE_URL:
         if notifier_dict := await DbManager().get_incomplete_tasks():
             for cid, data in notifier_dict.items():
